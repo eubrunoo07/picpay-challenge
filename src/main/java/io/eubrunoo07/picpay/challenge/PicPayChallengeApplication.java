37e77a2c8664a5f89a29b4a181fcb6e06f946952
eubrunoo07 <devbruninn@outlook.com>
2024-09-08 13:26:25 -0300

    first commit

---
+++ src/main/java/io/eubrunoo07/picpay/challenge/PicPayChallengeApplication.java
@@ -0,0 +1,13 @@
+package io.eubrunoo07.picpay.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPayChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPayChallengeApplication.class, args);
+	}
+
+}
